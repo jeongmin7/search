6d39111887db496b6291bb50abcb795bd64e3f1a
jeongmin <lee.jeongmin1625@gmail.com>
2023-01-12 11:59:19 +0900

    Remove: utils 삭제

--- src/utils/highlightText.ts
+++
@@ -1,6 +0,0 @@
-const highlightText = (text: string, searchTerm: string) => {
-  const isMatchedTextRegExp = new RegExp(searchTerm, 'g');
-  return text.replace(isMatchedTextRegExp, `<strong>${searchTerm}</strong>`);
-};
-
-export default highlightText;
